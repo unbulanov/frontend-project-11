edb8462da81103524d2e0683c8a30f57df948ff5
Ekaterina Ivanova <unbulanov@gmail.com>
2023-07-08 19:19:28 +0300

    use try catch, del if construct

--- src/parser.js
+++ src/parser.js
@@ -1,49 +1,27 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import uniqueId from 'lodash/uniqueId';
 
-export default (state, data, type, currentId) => {
+export default (state, data, currentId) => {
   try {
     const parser = new DOMParser();
     const document = parser.parseFromString(data.contents, 'text/xml');
     const items = document.querySelectorAll('item');
-    if (type === 'new') {
-      const channel = document.querySelector('channel');
-      const mainTitle = channel.querySelector('title').textContent;
-      const mainDescription = channel.querySelector('description').textContent;
-      state.feeds.push({
-        id: currentId, title: mainTitle, description: mainDescription,
-      });
+    const channel = document.querySelector('channel');
+    const mainTitle = channel.querySelector('title').textContent;
+    const mainDescription = channel.querySelector('description').textContent;
+    state.feeds.push({
+      id: currentId, title: mainTitle, description: mainDescription,
+    });
 
-      items.forEach((item) => {
-        const title = item.querySelector('title').textContent;
-        const description = item.querySelector('description').textContent;
-        const link = item.querySelector('link').textContent;
-        const uniqId = uniqueId();
-        state.posts.push({
-          feedId: currentId, id: uniqId, title, description, link,
-        });
+    items.forEach((item) => {
+      const title = item.querySelector('title').textContent;
+      const description = item.querySelector('description').textContent;
+      const link = item.querySelector('link').textContent;
+      const uniqId = uniqueId();
+      state.posts.push({
+        feedId: currentId, id: uniqId, title, description, link,
       });
-    }
-    if (type === 'existing') {
-      const existingPosts = state.posts.filter(({ feedId }) => feedId === currentId);
-      const existingPostsTitles = existingPosts.map(({ title }) => title);
-      const newPosts = Array.from(items).filter((item) => {
-        const title = item.querySelector('title').textContent;
-        return !existingPostsTitles.includes(title);
-      });
-      newPosts.forEach((post) => {
-        const title = post.querySelector('title').textContent;
-        const description = post.querySelector('description').textContent;
-        const link = post.querySelector('link').textContent;
-        const uniqId = uniqueId();
-        state.trackingPosts.push({
-          feedId: currentId, id: uniqId, title, description, link,
-        });
-        state.posts.push({
-          feedId: currentId, id: uniqId, title, description, link,
-        });
-      });
-    }
+    });
   } catch (err) {
     state.parsingErrors.push(err);
     throw new Error();
--- src/refresh.js
+++ src/refresh.js
@@ -6,7 +6,7 @@ export default (state, url, feedId) => {
   const modifiedUrl = `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`;
   const iter = () => {
     axios.get(modifiedUrl)
-      .then((response) => parser(state, response.data, 'existing', feedId))
+      .then((response) => parser(state, response.data, feedId))
       .catch((err) => console.error(err))
       .then(() => setTimeout(() => iter(), 5000));
   };
